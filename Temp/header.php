0aa824b7af52e8e90f72ba66187990274be7ae81
hkumbhar <kumbharhmt@gmail.com>
2017-09-14 16:27:31 +0530

    Welkin_live_Backup

---
+++ Temp/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='shortcut icon' type='image/x-icon' href='./images/favicon.ico' />
+     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"> 
+     <link href="css/style.css" rel="stylesheet" type="text/css" >
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Marcellus+SC" rel="stylesheet"> 
+</head>
---
+++ Temp/js/portfolio.js
@@ -0,0 +1,7 @@
+ 
+/*Redirect on same page click on close button*/
+
+ function openWindow() {
+        var win = window.open('portfolio.html',"_self");
+        win.focus();
+    }
